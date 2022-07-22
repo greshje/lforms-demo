102712db5550235c6ee452930e2fda3e4f4d799c
greshje <greshje@gmail.com>
2022-07-21 22:49:18 -0400

    still working

--- node_modules/fhirpath/browser-build/package-json-loader.js
+++
@@ -1,6 +0,0 @@
-// Removes all content except version.
-// When we import package.json we need only the application version.
-module.exports = function loader(source) {
-  const { version } = JSON.parse(source);
-  return JSON.stringify({ version });
-};
